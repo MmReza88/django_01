6e8f8439d980adc2ee5c6cf7b48dea553f106445
haarleme <eric.haarlemmer@grenoble-inp.org>
2025-04-02 16:41:48 +0200

    added view

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,11 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from .models import Totem
+
 @api_view(["GET"])
 def get_totem_infos(request, totem_id):
-    print(totem_id)
-    data = {"message": "Hello, DRF!"}
+    totem = Totem.objects.get(id=totem_id)
+    
+    data = {"parking_name": totem.parking.name, "durations": totem.get_durations(), "prices": totem.get_prices()}
     return Response(data)
