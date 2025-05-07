ba4acfcb9228a297e590758c24595b51ad3f53c3
MmReza88 <mr.sarrami.official@gmail.com>
2025-05-07 17:28:52 +0200

    before lab

--- pages/views.py
+++ pages/views.py
@@ -3,14 +3,17 @@ from rest_framework.decorators import api_view
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 import park_auth.api_functions as api
-from pages import views 
+from pages.models import Car, User_developed, User,Totem
+
 @api_view(['GET']) 
 def start_card(request, totem_id, secret_token, card_info):
     
     #TODO: Check it the secret_token is valid for the totem_id
     try:
         numeric_id = totem_id.split('_')[1]
-        totem = views.Totem.objects.select_related("parking__zone").get(identity_code=numeric_id)
+        totem = Totem.objects.select_related("parking__zone").get(identity_code=numeric_id)
+        print(f"Totem: {totem}")
+        print(f"Totem id: nu")
         if totem.secret_token != secret_token:
             return Response({"type": "error", "error": "Invalid secret token."})
         channel_layer = get_channel_layer()
@@ -20,7 +23,8 @@ def start_card(request, totem_id, secret_token, card_info):
                 "type": "send_start",  # this matches a method name in your consumer
             }
         )
-    except:
+    except Exception as e:
+        print(f"Error: {e}")
         return Response({"type": "error", "error": "Something is not good"})
     
     return Response({"type": "start", "secret_token": secret_token, "card_info": card_info})
