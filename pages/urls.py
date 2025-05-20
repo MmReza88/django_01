0f5f5ca641d58006ee2ff8c0ad0af8ccecb08399
MmReza88 <mr.sarrami.official@gmail.com>
2025-05-20 23:53:33 +0200

    .

--- pages/urls.py
+++ pages/urls.py
@@ -6,6 +6,5 @@ urlpatterns = [
     path('totem/<str:totem_id>/<str:secret_token>/', views.get_totem_infos, name = 'get_totem_infos'),
     path('user/cars/<str:username>/', views.get_user_cars, name='get_user_cars'),
     path('new-ticket/<int:duration>/<str:price>/<str:totem_id>/<str:secret_token>/<str:plate>/<str:card_number>/', views.new_ticket, name='new_ticket'),
-    
     path('car-status/<str:plate>/', views.get_car_parking_status, name='get_car_parking_status'),
 ]
