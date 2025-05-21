2927a78a9bf9138236fc51a39bbfe1bb7be9914a
haarleme <eric.haarlemmer@grenoble-inp.org>
2025-05-21 14:52:38 +0200

    fix bad import

--- control_admin/consumers.py
+++ control_admin/consumers.py
@@ -1,6 +1,5 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from . import api_functions
 
 #-------------------------------------------------------------------------------------
 
