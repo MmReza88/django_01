80aea38be3ba67bbc1406f6c6c35859e3a5ac33e
haarleme <eric.haarlemmer@grenoble-inp.org>
2025-06-14 19:27:55 +0200

    admin works perfecly but still some polish to do

--- pages/admin.py
+++ pages/admin.py
@@ -24,31 +24,115 @@ class BadgeAdmin(admin.ModelAdmin):
         super().save_model(request, obj, form, change)
 
 class FineAdmin(admin.ModelAdmin):
-    pass
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        user = request.user
+
+        if user.groups.filter(name="Customer_admin").exists():
+            try:
+                sp = user.user_developed.service_provider
+                return qs.filter(service_provider=sp)
+            except User_developed.DoesNotExist:
+                return qs.none()
+        return qs
+    
+    def save_model(self, request, obj, form, change):
+        if request.user.groups.filter(name="Customer_admin").exists():
+            try:
+                obj.service_provider = request.user.user_developed.service_provider
+            except User_developed.DoesNotExist:
+                pass # do nothing if the ca does not have any service provider
+        super().save_model(request, obj, form, change)
 
 class ChalkAdmin(admin.ModelAdmin):
-    pass
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        user = request.user
+
+        if user.groups.filter(name="Customer_admin").exists():
+            try:
+                sp = user.user_developed.service_provider
+                return qs.filter(service_provider=sp)
+            except User_developed.DoesNotExist:
+                return qs.none()
+        return qs
+    
+    def save_model(self, request, obj, form, change):
+        if request.user.groups.filter(name="Customer_admin").exists():
+            try:
+                obj.service_provider = request.user.user_developed.service_provider
+            except User_developed.DoesNotExist:
+                pass # do nothing if the ca does not have any service provider
+        super().save_model(request, obj, form, change)
 
 class TicketAdmin(admin.ModelAdmin):
-    pass
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        user = request.user
+
+        if user.groups.filter(name="Customer_admin").exists():
+            try:
+                sp = user.user_developed.service_provider
+                return qs.filter(Parking__zone__service_provider=sp)
+            except User_developed.DoesNotExist:
+                return qs.none()
+        return qs
 
 class ZoneAdmin(admin.ModelAdmin):
-    pass
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        user = request.user
+
+        if user.groups.filter(name="Customer_admin").exists():
+            try:
+                sp = user.user_developed.service_provider
+                return qs.filter(service_provider=sp)
+            except User_developed.DoesNotExist:
+                return qs.none()
+        return qs
+    
+    def save_model(self, request, obj, form, change):
+        if request.user.groups.filter(name="Customer_admin").exists():
+            try:
+                obj.service_provider = request.user.user_developed.service_provider
+            except User_developed.DoesNotExist:
+                pass # do nothing if the ca does not have any service provider
+        super().save_model(request, obj, form, change)
 
 class ParkingAdmin(admin.ModelAdmin):
-    pass
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        user = request.user
+
+        if user.groups.filter(name="Customer_admin").exists():
+            try:
+                sp = user.user_developed.service_provider
+                return qs.filter(zone__service_provider=sp)
+            except User_developed.DoesNotExist:
+                return qs.none()
+        return qs
 
 class TotemAdmin(admin.ModelAdmin):
-    pass
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        user = request.user
+
+        if user.groups.filter(name="Customer_admin").exists():
+            try:
+                sp = user.user_developed.service_provider
+                return qs.filter(parking__zone__service_provider=sp)
+            except User_developed.DoesNotExist:
+                return qs.none()
+        return qs
 
 admin.site.register(Badge, BadgeAdmin)
 admin.site.register(Car)
 admin.site.register(Service_provider)
 admin.site.register(City)
-admin.site.register(Zone)
-admin.site.register(Parking)
-admin.site.register(Totem)
-admin.site.register(Ticket)
+admin.site.register(Zone, ZoneAdmin)
+admin.site.register(Parking, ParkingAdmin)
+admin.site.register(Totem, TotemAdmin)
+admin.site.register(Ticket, TicketAdmin)
 admin.site.register(User_developed)
-admin.site.register(Fine)
-admin.site.register(Chalk)
+admin.site.register(Fine, FineAdmin)
+admin.site.register(Chalk, ChalkAdmin)
