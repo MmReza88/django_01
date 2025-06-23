77e63b630bf0a3f4b7ee5bc1042b140e1c61c2c6
haarleme <eric.haarlemmer@grenoble-inp.org>
2025-06-23 16:51:30 +0200

    s

--- local/rfid_controler.py
+++ local/rfid_controler.py
@@ -5,13 +5,12 @@ import time
 server_ip = "192.168.30.224"
 server_port = 8000
 totem_id = "controler_123"
-secret_token = "aevJFz4xFHNUK0gCohc1"
 
 while True:
     reader = SimpleMFRC522()
     id, _ = reader.read()
 
-    print(f"http://{server_ip}:{server_port}/ctrl/badge/{totem_id}{id}/")
+    print(f"http://{server_ip}:{server_port}/ctrl/badge/{totem_id}/{id}/")
     response = requests.get(f"http://{server_ip}:{server_port}/ctrl/badge/{totem_id}{id}/")
     print(response.text)
     
