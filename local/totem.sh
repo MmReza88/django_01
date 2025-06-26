85cdded38f489b9e0d540b2fe26b22b8bd027102
haarleme <eric.haarlemmer@grenoble-inp.org>
2025-06-26 18:24:39 +0200

    SQDFGDSFGHFDZAZQDSFG

--- local/totem.sh
+++ local/totem.sh
@@ -1,3 +1,5 @@
 python -m http.server 8000 --directory totem/ &
 python rfid_totem.py &
+rm -rf ~/.cache/mozilla/firefox
+rm -rf ~/.mozilla/firefox/*.default-release/cache2
 DISPLAY=:0 firefox-esr --kiosk http://localhost:8000/index.html
